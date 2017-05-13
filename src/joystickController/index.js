5bde716d42c3fd5280a753025d7894a0c7a554d3
Joshua Buchacher <jbuchacher@me.com>
2017-05-13 14:37:52 -0700

    Dont require a connection before emitting joystick events

--- src/joystickController/index.js
+++ src/joystickController/index.js
@@ -1,32 +1,26 @@
 var senseJoystick = require('sense-joystick')
 
 function JoystickController(io) {
-  io.on('connection', function (socket) {
-    socket.on('disconnect', function () {
-      socket.removeAllListeners();
-    })
+  senseJoystick.getJoystick()
+               .then((joystick) => {
+	         joystick.on('press', (direction) => {
+	           console.log('Got button press in the direction: ', direction);
 
-    senseJoystick.getJoystick()
-      .then((joystick) => {
-	joystick.on('press', (direction) => {
-	  console.log('Got button press in the direction: ', direction);
+                   var directionToInstrumentMapping = {
+                     left: 'hihat',
+                     right: 'crash',
+                     up: 'bd',
+                     down: 'snare',
+                     click: 'boobooboobooooooooo',
+                   }
 
-          var directionToInstrumentMapping = {
-            left: 'hihat',
-            right: 'crash',
-            up: 'bd',
-            down: 'snare',
-            click: 'boobooboobooooooooo',
-          }
-
-          var instrument = directionToInstrumentMapping[direction]
-          console.log("emitting: ", instrument)
-          io.emit(instrument)
-	});
-      }).catch((e) => {
-        console.log("Didn't find a PI hat")
-      })
-  });
+                   var instrument = directionToInstrumentMapping[direction]
+                   console.log("emitting: ", instrument)
+                   io.emit(instrument)
+	         });
+               }).catch((e) => {
+                 console.log("Didn't find a PI hat")
+               });
 }
 
 
