03f649b1de0e10bdb9acd7c4f021353d33ec1c93
Joshua Buchacher <jbuchacher@me.com>
2017-05-13 11:39:08 -0700

    Configure server via environment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var socketIoServerAddr = process.env['SOCKET_IO_SERVER_ADDRESS'] || '127.0.0.1';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', socketIoServerAddr: '127.0.0.1'});
+  res.render('index', { title: 'Express', socketIoServerAddr });
 });
 
 module.exports = router;
